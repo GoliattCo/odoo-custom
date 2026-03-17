3cd45657af35b16dfd769aa87747ec8f8d08f499
remcaro-rgb <remcaro@gmail.com>
2026-03-17 12:08:44 -0500

    feat(account_balance_sheet): scaffold module skeleton

---
+++ custom-addons/account_balance_sheet/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Balance Sheet Report',
+    'version': '18.0.1.0.0',
+    'summary': 'Live balance sheet report with PDF and Excel export',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_balance_sheet_views.xml',
+        'report/account_balance_sheet_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
